da7b10004fc30309b5643b8e62389b40c6df7ee5
Andres Armendariz <andres.armendariz@epn.edu.ec>
2023-07-31 18:03:48 -0500

    Fix error

--- 01 - Android/app/src/main/java/com/example/movilescomputacion2023a/MainActivity.kt
+++ 01 - Android/app/src/main/java/com/example/movilescomputacion2023a/MainActivity.kt
@@ -96,6 +96,18 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener {
                 irActividad(FRecyclerView::class.java)
             }
+
+        val botonGoogleMaps = findViewById<Button>(R.id.btn_google_maps)
+        botonGoogleMaps
+            .setOnClickListener {
+                irActividad(GGoogleMapsActivity::class.java)
+            }
+
+        val botonFirebaseUI = findViewById<Button>(R.id.btn_intent_firebase_ui)
+        botonFirebaseUI
+            .setOnClickListener {
+                irActividad(HFirebaseUIAuth::class.java)
+            }
     }
     fun abrirActividadConParametros(
         clase: Class<*>
